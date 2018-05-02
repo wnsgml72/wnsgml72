3244720587fe10bbaad98e3eaae9ef5ae81cc6be
wnsgmlhkhk <wnsgmlhkhk@gmail.com>
2018-05-02 12:27:44 +0900

    ㅇㅇㅇ

--- LEDELD/LEDELD/main.cpp
+++ LEDELD/LEDELD/main.cpp
@@ -5,18 +5,24 @@
  * Author : stc_164
  */ 
 
+#define F_CPU 16000000UL
 #include <avr/io.h>
+#include <util/delay.h>
 
 
 int main(void)
-{
+{	
+	DDRD=0XFF;
+	PORTD=0XFF;
+	int i=0;
     /* Replace with your application code */
     while (1) 
     {
+		for(i;i<7;i++){
+		PORTD&=~(1<<i);
+		_delay_ms(200);
+	}
 
-		printf("cccccccc");
-		printf("xxxxxxxxxxxxx");
-		printf("ddd");
     }
 }
 
